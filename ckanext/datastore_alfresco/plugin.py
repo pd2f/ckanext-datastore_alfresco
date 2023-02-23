b2446ce3c278cbf3d34de115b689fb9c0a7d94f8
Paulo Franco <paulo.franco@ccee.org.br>
2023-02-23 00:59:05 -0300

    novo teste

--- ckanext/datastore_alfresco/plugin.py
+++ ckanext/datastore_alfresco/plugin.py
@@ -1,14 +1,11 @@
-# encoding: utf-8
-
 import ckan.plugins as plugins
-
-
-def group_create(context, data_dict=None):
-    return {'success': False, 'msg': 'nao pode'}
+from ckanext.datastore.interfaces import IDatastoreBackend
+from ckanext.datastore_alfresco.alfresco_rest import DatastoreAlfrescoBackend
 
 
 class DatastoreAlfrescoPlugin(plugins.SingletonPlugin):
-    plugins.implements(plugins.IAuthFunctions)
-
-    def get_auth_functions(self):
-        return {'group_create': group_create}
+    plugins.implements(IDatastoreBackend)
+    
+    
+    def register_backends(self):
+        return {u'alfresco': DatastoreAlfrescoBackend}
